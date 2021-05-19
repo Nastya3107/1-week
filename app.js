db46cd34cb83b79ec1b4dbf45f318fd37541e848
Nastya3107 <nastya1990otc@gmail.com>
2021-05-19 19:12:39 +0300

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+// числа;
+value = Math.PI;
+value= value.toFixed(2);
+value = Math.min(15,11,16,12,51,12,13,51);
+value = Math.max(15,11,16,12,51,12,13,51);
+value = Math.random();
+value=value.toFixed(2);
+value= Math.floor(Math.random()*20+1);
+value= 0.6+0.7;
+value=value.toFixed(1);
+value= '100$';
+value= parseFloat("100$");
+//  строки;
+ let string = 'some test string';
+let str;
+str = string[0];
+str = string [string.length-1];
+str = string.toUpperCase();
+str= string.indexOf('string');
+str= string.lastIndexOf(' ');
+str=string.slice(5,9);
+str= string.slice(5,9);
+str= string.slice(0,-6);
+let a= 20;
+let b= 16;
+b="16";
+str = a+b;
+// обьекты;
+let obj ={
+  product:'iphone',
+};
+obj.price=1000;
+obj.currency='dollar';
+obj.details={};
+obj.details.model='M';
+obj.details.color=' black';
+
+console.log(obj);
